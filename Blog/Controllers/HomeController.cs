4ad825737db73cc670a45387bb90980470e8e1d5
Cory Beesler <cbeesler68@gmail.com>
2019-03-11 18:26:23 -0500

    Added ASP.net MVC project
    Added one home page for blog posts.

---
+++ Blog/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Post> model = new List<Post>();
+            SqlConnection conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB; Initial Catalog=Blog;Integrated Security=SSPI");
+
+            SqlDataReader rdr = null;
+
+            // 2. Open the connection
+            conn.Open();
+
+            // 3. Pass the connection to a command object
+            SqlCommand cmd = new SqlCommand("SELECT PostId, Title, Body = SUBSTRING(Body, 1, 100), Link, DatePosted FROM Post ORDER BY DatePosted DESC", conn);
+
+            // get query results
+            rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                var post = new Post();
+
+                post.PostId = (int)rdr["PostId"];
+                post.Title = rdr["Title"].ToString();
+                post.Body = rdr["Body"].ToString();
+                post.Link = rdr["Link"].ToString();
+                post.DatePosted = (DateTime)rdr["DatePosted"];
+
+                model.Add(post);
+            }
+
+            // close the reader
+            if (rdr != null)
+            {
+                rdr.Close();
+            }
+
+            // 5. Close the connection
+            if (conn != null)
+            {
+                conn.Close();
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Blog/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Link { get; set; }
+        public DateTime DatePosted { get; set; }
+    }
+}
